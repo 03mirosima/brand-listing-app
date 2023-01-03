8ec9298427ade6a6dc95a1cb61440a71d4adba48
03mirosima <sezenmiray2009@hotmail.com>
2023-01-03 21:27:39 +0300

    Created project file structure

--- src/index.js
+++ src/index.js
@@ -1,2 +1,13 @@
-const a = { name: "dd" };
-console.log(a);
+import React from "react";
+import ReactDOM from "react-dom";
+import "./styles/index.scss";
+import App from "./components/App";
+import store from "./store/store.js";
+import { Provider } from "react-redux";
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,27 @@
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   mode: "development",
+  output: {
+    assetModuleFilename: "images/[hash][ext][query]",
+    clean: true,
+  },
   module: {
     rules: [
+      { test: /\.(png|jpe?g|gif|svg)$/i, type: "asset" },
       {
-        test: /\.s?css$/i,
-        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+        test: /\.(s[ac]|c)ss$/i,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+          },
+          "css-loader",
+          "sass-loader",
+        ],
       },
       {
-        test: /\.js$/,
+        test: /\.jsx?$/,
         exclude: /node_modules/,
         use: {
           loader: "babel-loader",
@@ -17,7 +29,11 @@ module.exports = {
       },
     ],
   },
-  plugins: [new MiniCssExtractPlugin()],
+  plugins: [
+    new MiniCssExtractPlugin(),
+    new HtmlWebpackPlugin({ template: "./src/index.html" }),
+  ],
+  resolve: { extensions: [".js", ".jsx"] },
   devtool: "source-map",
   devServer: {
     static: "./dist",
